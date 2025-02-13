98dc1b16a695dc82f015cd8da5f3dd7a9451788b
V. Kartik Chundru <chundruvk16@gmail.com>
2025-02-13 10:59:54 +0000

    add links

--- app.R
+++ app.R
@@ -6,49 +6,68 @@ library(ggplot2)
 library(R.utils)
 library(data.table)
 library(readr)
+library(shinydisconnect)
 
 transcripts<-fread('https://github.com/chundruv/LRBrainTranscriptCoverage/raw/refs/heads/main/transcripts.txt', stringsAsFactors = F, data.table = F)
 
 # Define UI
 ui <- fluidPage(
-    titlePanel("Long read brain pext score"),
+    disconnectMessage(text = "App disconnected. Please see https://github.com/chundruv/LRBrainTranscriptCoverage. Run the app.R locally, no need to download data files",
+                      refresh = "",
+                      background = "#646464e6",
+                      size = 36,
+                      width = "full",
+                      top = "center",
+                      colour = "white",
+                      overlayColour = "#999",
+                      overlayOpacity = 0.4),
+    titlePanel("Long read brain coverage"),
     theme = shinythemes::shinytheme("united"),
     # Sidebar for inputs
     page_sidebar(
         sidebar = sidebar(width = 250,
-            textInput("gene", label = "Gene Name", value = "MYCBP2"),
-            checkboxGroupInput(
-                "Groups1", 
-                label = "Broad developmental groups", 
-                choices = c(
-                    "Total expression" = 'total_exp',
-                    "Prenatal expression" = 'prenatal_exp',
-                    "Postnatal expression" = 'postnatal_exp'
-                ),
-                selected = c('total_exp', 'prenatal_exp', 'postnatal_exp')
-            ),
-            checkboxGroupInput(
-                "Groups2", 
-                label = "Fine-scale prenatal developmental groups", 
-                choices = c(
-                    "1st trimester expression" = 'prenatal1sttrimester_exp',
-                    "2nd trimester expression" = 'prenatal2ndtrimester_exp',
-                    "3rd trimester expression" = 'prenatal3rdtrimester_exp'
-                ),
-                selected = c()
-            ),
-            checkboxGroupInput(
-                "Groups3", 
-                label = "Fine-scale postnatal developmental groups", 
-                choices = c(
-                    "Child expression" = 'postnatalchild_exp',
-                    "Adult expression" = 'postnataladult_exp',
-                    "Older adult expression" = 'postnatalelderly_exp'
-                ),
-                selected = c()
-            ),
-            actionButton("submit", "Submit!", icon("paper-plane"),style="color: #fff; background-color: #337ab7; border-color: #2e6da4"),
-            radioButtons("rb", "Scale", choiceNames = list("Linear", "Log"), choiceValues = list("linear", "log")) 
+                          textInput("gene", label = "Gene Name", value = "MYCBP2"),
+                          checkboxGroupInput(
+                              "Groups1", 
+                              label = "Broad developmental groups", 
+                              choices = c(
+                                  "Total expression" = 'total_exp',
+                                  "Prenatal expression" = 'prenatal_exp',
+                                  "Postnatal expression" = 'postnatal_exp'
+                              ),
+                              selected = c('total_exp', 'prenatal_exp', 'postnatal_exp')
+                          ),
+                          checkboxGroupInput(
+                              "Groups2", 
+                              label = "Fine-scale prenatal developmental groups", 
+                              choices = c(
+                                  "1st trimester expression" = 'prenatal1sttrimester_exp',
+                                  "2nd trimester expression" = 'prenatal2ndtrimester_exp',
+                                  "3rd trimester expression" = 'prenatal3rdtrimester_exp'
+                              ),
+                              selected = c()
+                          ),
+                          checkboxGroupInput(
+                              "Groups3", 
+                              label = "Fine-scale postnatal developmental groups", 
+                              choices = c(
+                                  "Child expression" = 'postnatalchild_exp',
+                                  "Adult expression" = 'postnataladult_exp',
+                                  "Older adult expression" = 'postnatalelderly_exp'
+                              ),
+                              selected = c()
+                          ),
+                          actionButton("submit", "Submit!", icon("paper-plane"),style="color: #fff; background-color: #337ab7; border-color: #2e6da4"),
+                          radioButtons("rb", "Scale", choiceNames = list("Linear", "Log"), choiceValues = list("linear", "log")),
+                          tags$footer(tags$a(img(src="https://github.githubassets.com/assets/GitHub-Mark-ea2971cee799.png", width="50",
+                                     height="50"),
+                                 href="https://github.com/chundruv/LRBrainTranscriptCoverage"),
+                          tags$a(img(src="https://github.com/chundruv/LRBrainTranscriptCoverage/raw/refs/heads/main/logos/paradigm.png", 
+                                     width="140", height="70"),
+                                 href="https://paradigmgenomics.org"),
+                          tags$a(img(src="https://github.com/chundruv/LRBrainTranscriptCoverage/raw/refs/heads/main/logos/hdr-cpg.png", 
+                                     width="140",height="70"),
+                                 href="https://www.epigenomicslab.com/brainisoform/"))
         ),
         
         # Main panel for plot
@@ -63,9 +82,8 @@ ui <- fluidPage(
 )
 
 # Define server logic
-server <- function(input, output) {
-
-
+server <- function(input, output, session) {
+    
     # Reactive expression to load and process expression data for selected groups
     dataset <- eventReactive(input$submit, {
         x1 <- fread(paste0('https://github.com/chundruv/LRBrainTranscriptCoverage/raw/refs/heads/main/data/',input$gene,'.txt.gz'), stringsAsFactors=F, data.table=F)
@@ -114,23 +132,23 @@ server <- function(input, output) {
                         'postnatalchild_exp' = "Child expression",
                         'postnataladult_exp' = "Adult expression",
                         'postnatalelderly_exp' = "Older adult expression")
-
+            
             if(input$rb=='linear'){
                 p[[i]] <- plotly_build(plot_ly() %>% add_trace(dataset1(), x = dataset1()$midpos, y = dataset1()[,i], 
-                                       color=~I(j), width = 10, type = 'bar', name=k, hoverinfo = "text", hovertext= 
-                                       ~paste('<br><b>Position range</b>: ',dataset1()$chr,':',dataset1()$minpos,'-',dataset1()$maxpos,'<br>',
-                                             
-                                             '<br><b>Group</b>: ',i,'<br>',
-                                             
-                                             '<br><b>Mean PEXT</b>: ',format(round(dataset1()[,i], 4), nsmall = 4),'<br>'))%>%
+                                                               color=~I(j), width = 10, type = 'bar', name=k, hoverinfo = "text", hovertext= 
+                                                                   ~paste('<br><b>Position range</b>: ',dataset1()$chr,':',dataset1()$minpos,'-',dataset1()$maxpos,'<br>',
+                                                                          
+                                                                          '<br><b>Group</b>: ',i,'<br>',
+                                                                          
+                                                                          '<br><b>Mean PEXT</b>: ',format(round(dataset1()[,i], 4), nsmall = 4),'<br>'))%>%
                                            layout(hovermode = 'x unified', xaxis = list(
-                                           title = "",
-                                           showgrid = FALSE,
-                                           showticklabels = FALSE  # Hide axis labels and ticks
-                                       ),
-                                       yaxis = list(range=c(0,1),
-                                           title = "", tickmode="array", tickvals=c(0,0.25,0.5,0.75,1)
-                                       )))
+                                               title = "",
+                                               showgrid = FALSE,
+                                               showticklabels = FALSE  # Hide axis labels and ticks
+                                           ),
+                                           yaxis = list(range=c(0,1),
+                                                        title = "", tickmode="array", tickvals=c(0,0.25,0.5,0.75,1)
+                                           )))
             }else{
                 p[[i]] <- plotly_build(plot_ly() %>% add_trace(dataset1(), x = dataset1()$midpos, y = dataset1()[,i], 
                                                                color=~I(j), width = 10, type = 'bar', name=k, hoverinfo = "text", hovertext= 
@@ -149,34 +167,34 @@ server <- function(input, output) {
                                            )))
             }
         }
-
+        
         p1<-subplot(p, nrows = length(c(input$Groups1,input$Groups2,input$Groups3)), shareX = TRUE, shareY = TRUE)%>%
             layout(hovermode = 'x unified', xaxis = list(title = "", 
-            showgrid = FALSE,
-            showticklabels = FALSE
-        ),
-        yaxis = list(
-            title = ""
-        ), legend = list(orientation = 'h'))
+                                                         showgrid = FALSE,
+                                                         showticklabels = FALSE
+            ),
+            yaxis = list(
+                title = ""
+            ), legend = list(orientation = 'h'))
         
         
         p2<-plot_ly()
         for (i in 1:length(geneshapes())) {
             shape <- geneshapes()[[i]]
             p2 <- p2 %>% add_trace(
-                        type = "scatter",
-                        mode = "lines",
-                        x = c(shape$x0, shape$x1),
-                        y = c(shape$y0, shape$y1),
-                        showlegend=F,
-                        line = list(color = shape$line$color, width = shape$line$width),
-                        text = shape$text,  # Add hover text here
-                        hoverinfo = "text"   # Enable hoverinfo to display the 'text'
-                    )
+                type = "scatter",
+                mode = "lines",
+                x = c(shape$x0, shape$x1),
+                y = c(shape$y0, shape$y1),
+                showlegend=F,
+                line = list(color = shape$line$color, width = shape$line$width),
+                text = shape$text,  # Add hover text here
+                hoverinfo = "text"   # Enable hoverinfo to display the 'text'
+            )
         }
         p2<-p2%>%layout(shapes = geneshapes(), xaxis=list(showgrid = FALSE,showticklabels = TRUE,title=""), 
                         yaxis=list(title = "", showgrid = FALSE, showticklabels = FALSE))
-
+        
         if(transcripts[which(transcripts$gene_name==input$gene),3]=='+'){
             strand<-'+ (\u2192)'
         }else if(transcripts[which(transcripts$gene_name==input$gene),3]=='-'){
@@ -187,18 +205,18 @@ server <- function(input, output) {
         subplot(p1,p2, nrows=2, heights = c(0.9,0.1), shareX = T)%>%
             layout(title = list(text = paste0("Gene: ", input$gene, ';\tStrand: ', strand, "\nMANEselect: ", 
                                               transcripts[which(transcripts$gene_name==input$gene),2])), margin=list(t=50))
-
+        
     }, ignoreNULL = T)
     
     output$plot<-renderPlotly({plot()})
     text<-eventReactive(input$submit,{
         gn=transcripts[which(transcripts$gene_name==input$gene),]
         HTML("Number of reads = ", gn$nreads, 
-               "<br>Number of unique transcript isoforms = ", gn$ntranscripts, 
-               "<br>Number of FSM transcripts = ", gn$nFSM, " (", gn$nFSMreads, ' reads)', 
-               "<br>Number of ISM transcripts = ", gn$nISM, " (", gn$nISMreads, ' reads)',
-               "<br>Number of NIC transcripts = ", gn$nNIC, " (", gn$nNICreads, ' reads)',
-               "<br>Number of NNC transcripts = ", gn$nNNC, " (", gn$nNNCreads, ' reads)',)}, ignoreNULL = T)
+             "<br>Number of unique transcript isoforms = ", gn$ntranscripts, 
+             "<br>Number of FSM transcripts = ", gn$nFSM, " (", gn$nFSMreads, ' reads)', 
+             "<br>Number of ISM transcripts = ", gn$nISM, " (", gn$nISMreads, ' reads)',
+             "<br>Number of NIC transcripts = ", gn$nNIC, " (", gn$nNICreads, ' reads)',
+             "<br>Number of NNC transcripts = ", gn$nNNC, " (", gn$nNNCreads, ' reads)')}, ignoreNULL = T)
     output$text<-renderText({text()})
 }
 
